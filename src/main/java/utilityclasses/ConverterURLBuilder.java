3183f94cbb76e7e709a1468c2816bd403693dee6
JeramiahS <sanchezjeramiah@gmail.com>
2025-09-07 20:51:21 -0500

    BASE_URL no longer contains "?" in the String. It has been moved to the getBuiltURL method.

--- src/main/java/utilityclasses/ConverterURLBuilder.java
+++ src/main/java/utilityclasses/ConverterURLBuilder.java
@@ -1,7 +1,7 @@
 package utilityclasses;
 
 public final class ConverterURLBuilder {
-    private static final String BASE_URL = "https://api.fxratesapi.com/convert?";
+    private static final String BASE_URL = "https://api.fxratesapi.com/convert";
 
     /**
      * The getBuiltURL concatenates and returns a URL as a String
@@ -9,10 +9,10 @@ public final class ConverterURLBuilder {
      * @param fromCurrencyCode  The currency code of the country to convert from
      * @param toCurrencyCode    The currency code of the country to convert to
      * @param amount            The monetary value to convert from
-     * @return                  A formatted String which is the URL which gets sent in a GET request
+     * @return                  A URL as a String
      */
     public static String getBuiltURL(String fromCurrencyCode, String toCurrencyCode, double amount) {
-        return BASE_URL + "from=" + fromCurrencyCode + "&to=" + toCurrencyCode + "&amount=" + amount + "&format=tsv";
+        return BASE_URL + "?from=" + fromCurrencyCode + "&to=" + toCurrencyCode + "&amount=" + amount + "&format=tsv";
     }
 
 }
