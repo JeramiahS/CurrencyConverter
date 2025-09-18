ec1faa13bc4e6496d1ce3ba49b7302316c516b02
JeramiahS <sanchezjeramiah@gmail.com>
2025-09-17 19:51:14 -0500

    Added argIsDouble and codesAreThreeChars boolean methods to validate user input. Main method now checks if both values are true before executing the main statements.

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -7,22 +7,35 @@ import java.io.IOException;
 public class Application {
 
     public static void main(String[] args) {
-       try {
-           double value = Double.parseDouble(args[0]);
-           if(!(args[1].length() == 3 && args[2].length() == 3)) {
-               throw new IllegalArgumentException("Country codes must be 3 characters (ex. 'USD').");
-           } else {
-               System.out.printf("%.2f %s is %s %s",
-                       value,
-                       args[1],
-                       APIHandler.getConversionResult(value, args[1], args[2]),
-                       args[2]);
-           }
-       } catch(NumberFormatException _) {
-           System.out.println("Enter a numerical value as the first argument.");
-       } catch(IOException | InterruptedException | IllegalArgumentException e) {
-            System.out.println(e.getLocalizedMessage());
-       }
+        if(argIsDouble(args[0]) && codesAreThreeChars(args[1], args[2])) {
+            try {
+                double value = Double.parseDouble(args[0]);
+                System.out.printf("%.2f %s is %f %s",
+                        value,
+                        args[1],
+                        APIHandler.getConversionResult(value, args[1], args[2]),
+                        args[2]);
+            } catch(IOException | InterruptedException e) {
+                System.out.println(e.getLocalizedMessage());
+            }
+        }
+    }
+
+    private static boolean argIsDouble(String arg) {
+        try {
+            double _ = Double.parseDouble(arg);
+            return true;
+        } catch(NumberFormatException _) {
+            throw new NumberFormatException("You must enter a numerical value as the first argument (Ex. '10' or '20,00').");
+        }
+    }
+
+    private static boolean codesAreThreeChars(String arg1, String arg2) {
+        if((arg1.length() == 3 && arg2.length() == 3)) {
+            return true;
+        } else {
+            throw new IllegalArgumentException("Country codes must 3 characters (Ex. 'USD', 'EUR').");
+        }
     }
 
 }
