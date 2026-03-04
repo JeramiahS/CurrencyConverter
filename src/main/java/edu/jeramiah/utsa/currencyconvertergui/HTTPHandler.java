078578bd338f923f37c5a1b75b0a77d1511f14ba
JeramiahS <sanchezjeramiah@gmail.com>
2026-03-03 18:12:38 -0600

    Added HTTPHandler.java for interacting with the FXRates API.

---
+++ src/main/java/edu/jeramiah/utsa/currencyconvertergui/HTTPHandler.java
@@ -0,0 +1,54 @@
+package edu.jeramiah.utsa.currencyconvertergui;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Path;
+
+/**
+ * HTTPHandler contains the methods to send HTTP Requests to the currency converter server
+ *
+ * @author Jeramiah Sanchez
+ */
+public class HTTPHandler {
+
+    /**
+     * Sends a GET request containing the parameters to convert the given amount of currency.
+     * It stores the response in a JSON file.
+     *
+     * @param AMOUNT                The amount of money to convert
+     * @param FROM_CODE             The currency code to convert from
+     * @param TO_CODE               The currency code to convert to
+     * @throws IOException          Thrown when an IO exception occurs
+     * @throws InterruptedException Thrown when the connection to the server gets interrupted
+     */
+    public static void sendConversionRequest(final String AMOUNT, final String FROM_CODE, final String TO_CODE) throws IOException, InterruptedException {
+        try (final HttpClient HTTP_CLIENT = HttpClient.newHttpClient()) {
+            final String CURRENCY_CONVERTER_URL = String.format(
+                    "https://api.fxratesapi.com/convert?from=%s&to=%s&amount=%s",
+                    FROM_CODE,
+                    TO_CODE,
+                    AMOUNT
+            );
+            final HttpRequest REQUEST = HttpRequest.newBuilder().uri(URI.create(CURRENCY_CONVERTER_URL)).GET().build();
+            HTTP_CLIENT.send(REQUEST, HttpResponse.BodyHandlers.ofFile(Path.of("src/main/resources/edu/jeramiah/utsa/currencyconvertergui/files/response.json")));
+        }
+    }
+
+    /**
+     * Sends a GET request to receive a JSON file containing all FXRates supported currencies
+     *
+     * @throws IOException          Thrown when an IO exception occurs
+     * @throws InterruptedException Thrown when the connection to the server gets interrupted
+     */
+    public static void sendGetAllCurrenciesRequest() throws IOException, InterruptedException {
+        try (final HttpClient HTTP_CLIENT = HttpClient.newHttpClient()) {
+            final String GET_CURRENCIES_URL = "https://api.fxratesapi.com/currencies";
+            final HttpRequest REQUEST = HttpRequest.newBuilder().uri(URI.create(GET_CURRENCIES_URL)).GET().build();
+            HTTP_CLIENT.send(REQUEST, HttpResponse.BodyHandlers.ofFile(Path.of("src/main/resources/edu/jeramiah/utsa/currencyconvertergui/files/currencies.json")));
+        }
+    }
+
+}
