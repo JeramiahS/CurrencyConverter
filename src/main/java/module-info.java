5b01a0d7a7c10e271d0f59d9a1910a8f559ad722
JeramiahS <sanchezjeramiah@gmail.com>
2026-03-06 14:18:09 -0600

    module-info.java now requires tools.jackson.databind

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module edu.jeramiah.utsa.currencyconvertergui {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.net.http;
+    requires tools.jackson.databind;
 
 
     opens edu.jeramiah.utsa.currencyconvertergui to javafx.fxml;
